f27d9408bc1d064fb40566eae79be2edf42ba256
William Keith Pendleton <wkp23@byu.edu>
2024-10-24 02:23:00 -0600

    Initial Commit

---
+++ Language Learning App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Language Learning App
+//
+//  Created by William Pendleton on 10/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Language Learning App/Language_Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Language_Learning_AppApp.swift
+//  Language Learning App
+//
+//  Created by William Pendleton on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Language_Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
