c435d0dceaad99769f9e97f576e15bd9f18eae6a
William Keith Pendleton <wkp23@byu.edu>
2024-10-25 03:01:00 -0600

    added(confetti) to celebrate when the user completed the quiz! modify(QuizView) added visual feedback to show the user got the question correct or incorrect #16 #17

---
+++ Language Learning App/Views/Quiz/ConfettiView.swift
@@ -0,0 +1,18 @@
+//
+//  ConfettiView.swift
+//  Language Learning App
+//
+//  Created by William Pendleton on 10/25/24.
+//
+
+import SwiftUI
+
+struct ConfettiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfettiView()
+}
---
+++ Language Learning App/Views/Quiz/DisplayConfettiModifier.swift
@@ -0,0 +1,18 @@
+//
+//  DisplayConfettiModifier.swift
+//  Language Learning App
+//
+//  Created by William Pendleton on 10/25/24.
+//
+
+import SwiftUI
+
+struct DisplayConfettiModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DisplayConfettiModifier()
+}
